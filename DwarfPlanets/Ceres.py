d57c452038798b5b7e86838e72474c75c5be30ea
Billy <83192688+BDsProjects@users.noreply.github.com>
2025-04-22 13:17:48 +0100

    added basic info for Ceres
    Not all determinable atm without more research and sources, likely to be the case for all dwarf planets besides maybe pluto

---
+++ DwarfPlanets/Ceres.py
@@ -0,0 +1,26 @@
+r_ceres = 469.7  # km, volumetric mean radius of ceres
+
+# Orbital Parameters
+a_ceres = 446.00e6  # km
+p_ceres = 381.00e6  # km
+semi_major_ceres = 414.0e6 # km
+
+mean_orbital_velocity_ceres = 17.9 # km/s
+#Max_orbital_velocity_ceres = 5.47 # km/s Will need to find, above is from wikipedia
+#Min_orbital_velocity_ceres = 5.37 # km/s Will need to find, above is from wikipedia
+
+i_ceres = 1.770 # degrees, Inclination, what reference frame is this in? This is compared to Earth's 0.0 value
+e_ceres = 0.0785 # eccentricity
+
+
+# ceres Mean Orbital Elements (J2000) - JPL Small object center
+semi_major_ceres_J2000 = 30.06896348 # AU
+e_ceres_J2000 = 0.07940124623779937 # eccentricity
+i_ceres_J2000 = 10.58779899521626 # degrees
+longitude_ascending_node_ceres_J2000 = 80.25220287379196 # degrees
+longitude_perihelion_ceres_J2000 = 73.27344713151996 # degrees
+mean_anomaly_ceres_J2000 = 188.7026826097386 # degrees
+
+# # ceres Observation parameters
+# min_dist_frm_earth = 4319.0e6 # km, closest distance from Earth to ceres
+# max_dist_frm_earth = 4711.0e6 # km, farthest distance from Earth to ceres
