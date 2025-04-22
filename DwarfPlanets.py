08c2edbfd17dff4c1406b9114f46ac6f7308ced3
Billy <83192688+BDsProjects@users.noreply.github.com>
2025-04-22 08:26:37 +0100

    Ceres info and constants

--- DwarfPlanets.py
+++ DwarfPlanets.py
@@ -14,7 +14,11 @@
 # In order of distance
 
 # 1. Ceres
-
+a_ceres = 2.98 # AU
+p_ceres = 2.55 # AU
+s_major_ceres = 2.77 # AU
+orb_period_ceres = 4.60 # years
+orb_period_ceres_days = 1670 # days
 # 2. Pluto
 # 3. Haumea
 # 4. Makemake
