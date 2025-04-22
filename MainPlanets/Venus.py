2eeea0f118e1483169183900e5b59c8d028f10fc
Billy <83192688+BDsProjects@users.noreply.github.com>
2025-04-22 12:28:37 +0100

    added in venus base info

---
+++ MainPlanets/Venus.py
@@ -0,0 +1,26 @@
+r_venus = 6051.8  # km, volumetric mean radius of venus
+
+# Orbital Parameters
+a_venus = 108.941e6  # km
+p_venus = 107.480e6  # km
+semi_major_venus = 108.210e6 # km
+
+mean_orbital_velocity_venus = 35.02 # km/s
+Max_orbital_velocity_venus = 35.26 # km/s
+Min_orbital_velocity_venus = 34.78 # km/s
+
+i_venus = 7.395 # degrees, Inclination, what reference frame is this in? This is compared to Earth's 0.0 value
+e_venus = 0.0068 # eccentricity
+
+
+# venus Mean Orbital Elements (J2000)
+semi_major_venus_J2000 = 0.72333199 # AU
+e_venus_J2000 = 0.00677323 # eccentricity
+i_venus_J2000 = 3.39471 # degrees
+longitude_ascending_node_venus_J2000 = 76.68069 # degrees
+longitude_perihelion_venus_J2000 = 131.53298 # degrees
+mean_anomaly_venus_J2000 = 181.97973 # degrees
+
+# venus Observation parameters
+min_dist_frm_earth = 38.2e6 # km, closest distance from Earth to venus
+max_dist_frm_earth = 261.0e6 # km, farthest distance from Earth to venus
