b678d5b82d85c0437383c477f0f5504ff10e1fad
Billy <83192688+BDsProjects@users.noreply.github.com>
2025-04-22 12:41:02 +0100

    added Jupiter base info

---
+++ MainPlanets/Jupiter.py
@@ -0,0 +1,26 @@
+r_jupiter = 69,911  # km, volumetric mean radius of jupiter
+
+# Orbital Parameters
+a_jupiter = 816.363e6  # km
+p_jupiter = 740.595e6  # km
+semi_major_jupiter = 778.479e6 # km
+
+mean_orbital_velocity_jupiter = 13.06 # km/s
+Max_orbital_velocity_jupiter = 13.72 # km/s
+Min_orbital_velocity_jupiter = 12.44 # km/s
+
+i_jupiter = 1.304 # degrees, Inclination, what reference frame is this in? This is compared to Earth's 0.0 value
+e_jupiter = 0.0487 # eccentricity
+
+
+# jupiter Mean Orbital Elements (J2000)
+semi_major_jupiter_J2000 = 5.20336301 # AU
+e_jupiter_J2000 = 0.04839266 # eccentricity
+i_jupiter_J2000 = 1.30530 # degrees
+longitude_ascending_node_jupiter_J2000 = 100.55615 # degrees
+longitude_perihelion_jupiter_J2000 = 14.75385 # degrees
+mean_anomaly_jupiter_J2000 = 34.40438 # degrees
+
+# jupiter Observation parameters
+min_dist_frm_earth = 588.5e6 # km, closest distance from Earth to jupiter
+max_dist_frm_earth = 968.5e6 # km, farthest distance from Earth to jupiter
