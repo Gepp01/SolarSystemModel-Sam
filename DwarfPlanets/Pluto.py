f2fe396a6d143ecb2af8296affeb81d3fbce89c1
Billy <83192688+BDsProjects@users.noreply.github.com>
2025-04-22 13:26:48 +0100

    Added pluto base ino
    - also added OneNote pages for:
    -- Kuiper belt
    -- oort cloud
    -- asteroid belt
    -- Heliosphere

---
+++ DwarfPlanets/Pluto.py
@@ -0,0 +1,26 @@
+r_pluto = 1188  # km, volumetric mean radius of pluto
+
+# Orbital Parameters
+a_pluto = 7304.326e6  # km
+p_pluto = 4434.987e6  # km
+semi_major_pluto = 5869.656e6 # km
+
+mean_orbital_velocity_pluto = 4.64 # km/s
+Max_orbital_velocity_pluto = 6.10 # km/s Will need to find, above is from wikipedia
+Min_orbital_velocity_pluto = 3.71 # km/s Will need to find, above is from wikipedia
+
+i_pluto = 17.16 # degrees, Inclination, what reference frame is this in? This is compared to Earth's 0.0 value
+e_pluto = 0.2444 # eccentricity
+
+
+# pluto Mean Orbital Elements (J2000) - JPL Small object center
+semi_major_pluto_J2000 = 39.48168677 # AU
+e_pluto_J2000 = 0.24880766 # eccentricity
+i_pluto_J2000 = 17.14175 # degrees
+longitude_ascending_node_pluto_J2000 = 110.30347 # degrees
+longitude_perihelion_pluto_J2000 = 224.06676 # degrees
+mean_anomaly_pluto_J2000 = 238.92881 # degrees
+
+# pluto Observation parameters
+min_dist_frm_earth_pluto = 4284.7e6 # km, closest distance from Earth to pluto
+max_dist_frm_earth_pluto = 7528.0e6 # km, farthest distance from Earth to pluto
