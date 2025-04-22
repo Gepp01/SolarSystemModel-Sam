02d6d2940b89e85c6b690a03f4bd076bb61a49a4
Billy <83192688+BDsProjects@users.noreply.github.com>
2025-04-22 12:50:58 +0100

    Added base Uranus info

---
+++ MainPlanets/Uranus.py
@@ -0,0 +1,26 @@
+r_uranus = 25362  # km, volumetric mean radius of uranus
+
+# Orbital Parameters
+a_uranus = 3001.390e6  # km
+p_uranus = 2732.696e6  # km
+semi_major_uranus = 2867.043e6 # km
+
+mean_orbital_velocity_uranus = 6.79 # km/s
+Max_orbital_velocity_uranus = 7.13 # km/s
+Min_orbital_velocity_uranus = 6.49 # km/s
+
+i_uranus = 0.770 # degrees, Inclination, what reference frame is this in? This is compared to Earth's 0.0 value
+e_uranus = 0.0469 # eccentricity
+
+
+# uranus Mean Orbital Elements (J2000)
+semi_major_uranus_J2000 = 19.19126393 # AU
+e_uranus_J2000 = 0.04716771 # eccentricity
+i_uranus_J2000 = 0.76986 # degrees
+longitude_ascending_node_uranus_J2000 = 74.22988 # degrees
+longitude_perihelion_uranus_J2000 = 170.96424 # degrees
+mean_anomaly_uranus_J2000 = 313.23218 # degrees
+
+# uranus Observation parameters
+min_dist_frm_earth = 2580.6e6 # km, closest distance from Earth to uranus
+max_dist_frm_earth = 3153.5e6 # km, farthest distance from Earth to uranus
