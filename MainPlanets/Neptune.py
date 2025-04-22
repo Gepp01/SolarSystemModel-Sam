af437d41ea9ddc06f50311494f224602565c46be
Billy <83192688+BDsProjects@users.noreply.github.com>
2025-04-22 12:57:48 +0100

    added 2 ideas and base neptune info

---
+++ MainPlanets/Neptune.py
@@ -0,0 +1,26 @@
+r_neptune = 24622  # km, volumetric mean radius of neptune
+
+# Orbital Parameters
+a_neptune = 4514.953e6  # km
+p_neptune = 4558.857e6  # km
+semi_major_neptune = 4514.953e6 # km
+
+mean_orbital_velocity_neptune = 5.45 # km/s
+Max_orbital_velocity_neptune = 5.47 # km/s
+Min_orbital_velocity_neptune = 5.37 # km/s
+
+i_neptune = 1.770 # degrees, Inclination, what reference frame is this in? This is compared to Earth's 0.0 value
+e_neptune = 0.0097 # eccentricity
+
+
+# neptune Mean Orbital Elements (J2000)
+semi_major_neptune_J2000 = 30.06896348 # AU
+e_neptune_J2000 = 0.00858587 # eccentricity
+i_neptune_J2000 = 1.76917 # degrees
+longitude_ascending_node_neptune_J2000 = 131.72169 # degrees
+longitude_perihelion_neptune_J2000 = 44.97135 # degrees
+mean_anomaly_neptune_J2000 = 304.88003 # degrees
+
+# neptune Observation parameters
+min_dist_frm_earth = 4319.0e6 # km, closest distance from Earth to neptune
+max_dist_frm_earth = 4711.0e6 # km, farthest distance from Earth to neptune
