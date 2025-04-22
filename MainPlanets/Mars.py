f34beedd754ea27ac959573c6b94f9efc9b29a8d
Billy <83192688+BDsProjects@users.noreply.github.com>
2025-04-22 12:34:13 +0100

    added mars base info

---
+++ MainPlanets/Mars.py
@@ -0,0 +1,26 @@
+r_mars = 3389.5  # km, volumetric mean radius of mars
+
+# Orbital Parameters
+a_mars = 249.261e6  # km
+p_mars = 206.650e6  # km
+semi_major_mars = 227.956e6 # km
+
+mean_orbital_velocity_mars = 24.08 # km/s
+Max_orbital_velocity_mars = 26.50 # km/s
+Min_orbital_velocity_mars = 21.97 # km/s
+
+i_mars = 1.848 # degrees, Inclination, what reference frame is this in? This is compared to Earth's 0.0 value
+e_mars = 0.0935 # eccentricity
+
+
+# mars Mean Orbital Elements (J2000)
+semi_major_mars_J2000 = 1.52366231 # AU
+e_mars_J2000 = 0.09341233 # eccentricity
+i_mars_J2000 = 1.85061 # degrees
+longitude_ascending_node_mars_J2000 = 49.57854 # degrees
+longitude_perihelion_mars_J2000 = 336.04084 # degrees
+mean_anomaly_mars_J2000 = 355.45332 # degrees
+
+# mars Observation parameters
+min_dist_frm_earth = 54.6e6 # km, closest distance from Earth to mars
+max_dist_frm_earth = 401.4e6 # km, farthest distance from Earth to mars
