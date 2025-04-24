9e244701721a20694d65d11e077f06f5b09cea9d
Billy <83192688+BDsProjects@users.noreply.github.com>
2025-04-24 04:07:50 +0100

    added sedna basic info

---
+++ DwarfPlanets/Sedna.py
@@ -0,0 +1,28 @@
+#Sedna.py
+
+r_sedna = 453.0  # km, volumetric mean radius of sedna
+
+# Orbital Parameters
+a_sedna = 140173.204845e6  # km
+p_sedna = 11397.861768e6  # km
+semi_major_sedna =  75696.522574e6 # km
+
+#mean_orbital_velocity_sedna = 4.53 # km/s
+#Max_orbital_velocity_sedna = 5.47 # km/s Will need to find, above is from wikipedia
+#Min_orbital_velocity_sedna = 5.37 # km/s Will need to find, above is from wikipedia
+
+i_sedna = 11.9307 # degrees, Inclination, what reference frame is this in? This is compared to Earth's 0.0 value
+e_sedna = 0.8496 # eccentricity
+
+
+# sedna Mean Orbital Elements (J2000) - JPL Small object center
+semi_major_sedna_J2000 = 551.9042335362559 # AU
+e_sedna_J2000 = 0.4356754828731724 # eccentricity
+i_sedna_J2000 = 11.92675915751434 # degrees
+longitude_ascending_node_sedna_J2000 = 144.4409402177273 # degrees
+longitude_perihelion_sedna_J2000 = 310.9384922214027 # degrees, may need to check this one
+mean_anomaly_sedna_J2000 = 358.6049332262779 # degrees
+
+# # sedna Observation parameters
+# min_dist_frm_earth = 4319.0e6 # km, closest distance from Earth to sedna
+# max_dist_frm_earth = 4711.0e6 # km, farthest distance from Earth to sedna
