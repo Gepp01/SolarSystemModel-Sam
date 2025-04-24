0b6906e11dc680cad98e48c016b09428659b9664
Billy <83192688+BDsProjects@users.noreply.github.com>
2025-04-24 09:21:45 +0100

    added all planets page

---
+++ PlotlyDashSSv1/pages/all.py
@@ -0,0 +1,9 @@
+# pages/outer.py
+from dash import html, dcc, register_page
+import solar_helpers as sh
+
+register_page(__name__, path='/all', name='All')
+layout = html.Div([
+    html.H2("All Planets"),
+    dcc.Graph(figure=sh.build_fig(['Mercury','Venus','Earth','Ceres','Mars','Jupiter','Saturn','Uranus','Neptune','Pluto','Haumea','Makemake','Eris', 'Sedna'], 70, "All")),
+])
