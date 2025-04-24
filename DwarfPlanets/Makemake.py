ddc00fd6a98d57fecab57f8af8930231d7ea890c
Billy <83192688+BDsProjects@users.noreply.github.com>
2025-04-24 03:49:33 +0100

    added makemake base info

---
+++ DwarfPlanets/Makemake.py
@@ -0,0 +1,26 @@
+r_Makemake = 620.0  # km, volumetric mean radius of Makemake
+
+# Orbital Parameters
+a_Makemake = 7892.185266e6  # km
+p_Makemake = 5700.277265e6  # km
+semi_major_Makemake = 6796.231265e6 # km
+
+#mean_orbital_velocity_Makemake = 4.53 # km/s
+#Max_orbital_velocity_Makemake = 5.47 # km/s Will need to find, above is from wikipedia
+#Min_orbital_velocity_Makemake = 5.37 # km/s Will need to find, above is from wikipedia
+
+i_Makemake = 28.2137 # degrees, Inclination, what reference frame is this in? This is compared to Earth's 0.0 value
+e_Makemake = 0.19642 # eccentricity
+
+
+# Makemake Mean Orbital Elements (J2000) - JPL Small object center
+semi_major_Makemake_J2000 = 45.4494116430709 # AU
+e_Makemake_J2000 = 0.1619422080835269 # eccentricity
+i_Makemake_J2000 = 29.03385909047896 # degrees
+longitude_ascending_node_Makemake_J2000 = 79.25967485693748 # degrees
+longitude_perihelion_Makemake_J2000 = 296.9518517871766 # degrees, may need to check this one
+mean_anomaly_Makemake_J2000 = 168.8256590947274	 # degrees
+
+# # Makemake Observation parameters
+# min_dist_frm_earth = 4319.0e6 # km, closest distance from Earth to Makemake
+# max_dist_frm_earth = 4711.0e6 # km, farthest distance from Earth to Makemake
