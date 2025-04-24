73e27f16a69f4d77e9826394493eb46a0bf519a8
Billy <83192688+BDsProjects@users.noreply.github.com>
2025-04-24 09:52:04 +0100

    all.py page update
    solved range issue for all view
    - Sedna unsuprisingly skews it by a lot.

--- PlotlyDashSSv1/pages/all.py
+++ PlotlyDashSSv1/pages/all.py
@@ -1,9 +1,9 @@
-# pages/outer.py
+# pages/all.py
 from dash import html, dcc, register_page
 import solar_helpers as sh
 
 register_page(__name__, path='/all', name='All')
 layout = html.Div([
     html.H2("All Planets"),
-    dcc.Graph(figure=sh.build_fig(['Mercury','Venus','Earth','Ceres','Mars','Jupiter','Saturn','Uranus','Neptune','Pluto','Haumea','Makemake','Eris', 'Sedna'], 70, "All")),
+    dcc.Graph(figure=sh.build_fig(['Mercury','Venus','Earth','Ceres','Mars','Jupiter','Saturn','Uranus','Neptune','Pluto','Haumea','Makemake','Eris', 'Sedna'], 1000, "All")),
 ])
