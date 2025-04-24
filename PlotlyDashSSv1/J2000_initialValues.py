11d4934be4ed2236edc841b4ac09cd3a356f521e
Billy <83192688+BDsProjects@users.noreply.github.com>
2025-04-24 07:26:34 +0100

    Update J2000_initialValues.py
    added init structure

---
+++ PlotlyDashSSv1/J2000_initialValues.py
@@ -0,0 +1,26 @@
+# What is J2000 CS?
+
+
+
+
+# Characteristic Order below (Initial Non-Moving Values)
+
+# Main Planets
+# 1. Mercury
+# 2. Venus
+# 3. Earth
+# 4. Mars
+# 5. Jupiter
+# 6. Saturn
+# 7. Uranus
+# 8. Neptune
+
+# Dwarf Planets
+# 9.  Ceres
+# 10. Pluto
+# 11. Haumea
+# 12. Makemake
+# 13. Eris
+# 14. Sedna
+
+
